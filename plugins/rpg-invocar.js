95f458808553391cecef401b0ab913a9036f1acd
Aldair828 <aldairdurand6@gmail.com>
2024-10-04 13:20:23 -0500

    Update and rename rpg-shop.js to rpg-invocar.js

---
+++ plugins/rpg-invocar.js
@@ -0,0 +1,21 @@
+
+let handler = async (m, { conn, participants }) => {
+  let text = '🚩 *Invocando a todos los miembros del grupo* 🚩\n\n'
+  
+  // Recorrer la lista de participantes y agregar etiquetas
+  for (let member of participants) {
+    text += `@${member.id.split('@')[0]} `
+  }
+
+  // Enviar el mensaje con la mención a todos
+  await conn.sendMessage(m.chat, { text: text, mentions: participants.map(a => a.id) }, { quoted: m })
+}
+
+handler.help = ['invocar']
+handler.tags = ['group']
+handler.command = ['invocar', 'tagall', 'todos'] // Puedes usar varios nombres de comando
+
+handler.group = true // Solo funciona en grupos
+handler.admin = true // Solo administradores pueden usar el comando
+
+export default handler
