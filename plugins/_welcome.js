a8ac1ae14d81444d86ef89d8af1136f8fd2657f5
Aldair828 <aldairdurand6@gmail.com>
2024-10-07 21:49:38 -0500

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -1,29 +1,27 @@
-export async function before(m, {conn, participants, groupMetadata}) {
+import { WAMessageStubType } from '@whiskeysockets/baileys';
+import fetch from 'node-fetch';
+
+export async function before(m, { conn, participants, groupMetadata }) {
   if (!m.messageStubType || !m.isGroup) return !0;
-  
-  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => welcome);
-  let pp2 = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => adios);
-  let img = await (await fetch(${pp})).buffer();
-  let img2 = await (await fetch(${pp2})).buffer();
-
+  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://tinyurl.com/ylgu47w3');
+  let img = await (await fetch(`${pp}`)).buffer();
   let chat = global.db.data.chats[m.chat];
-  let user = global.db.data.users[m.messageStubParameters[0]] || {}; // Asumiendo que tienes una base de datos de usuarios
+
+  if (chat.bienvenida && m.messageStubType == 27) {
+    let bienvenida = `@${m.messageStubParameters[0].split`@`[0]} *BIENVENIDO A* ${groupMetadata.subject}\n\n*PUEDE LEER LAS REGLAS DEL GRUPO Y ASÍ EVITAR QUE TE ELIMINEN*\n${groupMetadata.desc}`;
+    
+    await conn.sendAi(m.chat, botname, textbot, bienvenida, img, img, canal, estilo);
+  }
   
-  let registered = user.registered ? "Sí" : "No"; // Comprueba si está registrado
-  let limit = user.limit || 0; // Asigna la cantidad de créditos como "limit"
-
-  if (chat.welcome && m.messageStubType == 27) { // Cuando un usuario entra
-    let wel =  `*BIENVENIDO @${m.messageStubParameters[0].split@[0]} AL GRUPO ${groupMetadata.subject}*\n\n*TU INFORMACIÓN*\n➢ Registrado: ${registered}\n➢ Créditos: ${limit}`;
-    await conn.sendMini(m.chat, packname, dev, wel, img, img, channel, fkontak);
+  if (chat.bienvenida && m.messageStubType == 28) {
+    let bye = `@${m.messageStubParameters[0].split`@`[0]} *SE FUE DEL GRUPO*`;
+    
+    await conn.sendAi(m.chat, botname, textbot, bye, img, img, canal, estilo);
   }
-
-  if (chat.welcome && m.messageStubType == 28) { // Cuando un usuario se va
-    let bye = *@${m.messageStubParameters[0].split@[0]} SALIÓ DEL GRUPO 👁‍🗨*;
-    await conn.sendMini(m.chat, packname, dev, bye, img2, img2, channel, fkontak);
-  }
-
-  if (chat.welcome && m.messageStubType == 32) { // Cuando un usuario es expulsado
-    let kick =  `*@${m.messageStubParameters[0].split@[0]} FUE EXPULSADO DEL GRUPO 👁‍🗨*`;
-    await conn.sendMini(m.chat, packname, dev, kick, img2, img2, channel, fkontak);
+  
+  if (chat.bienvenida && m.messageStubType == 32) {
+    let kick = `@${m.messageStubParameters[0].split`@`[0]} *SE FUE DEL GRUPO*`;
+    
+    await conn.sendAi(m.chat, botname, textbot, kick, img, img, canal, estilo);
   }
 }
