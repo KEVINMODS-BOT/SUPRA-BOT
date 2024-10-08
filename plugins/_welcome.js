dde2d4f789675ee4dd886a30df64b5a154be8282
Aldair828 <aldairdurand6@gmail.com>
2024-10-07 22:29:29 -0500

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -1,27 +1,41 @@
 import { WAMessageStubType } from '@whiskeysockets/baileys';
 import fetch from 'node-fetch';
 
-export async function before(m, { conn, participants, groupMetadata }) {
+export async function before(m, { conn, participants, groupMetadata, text, isAdmin }) {
   if (!m.messageStubType || !m.isGroup) return !0;
-  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://tinyurl.com/2c3kwdfl');
+  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://tinyurl.com/ylgu47w3');
   let img = await (await fetch(`${pp}`)).buffer();
   let chat = global.db.data.chats[m.chat];
 
+  // Comando .setwelcome para configurar la bienvenida personalizada
+  if (text.startsWith('.setwelcome') && isAdmin) {
+    let welcomeMessage = text.slice(11).trim();
+    chat.customWelcome = welcomeMessage;
+    await conn.sendMessage(m.chat, 'Mensaje de bienvenida configurado con éxito.', 'conversation');
+  }
+
+  // Comando .setbye para configurar el mensaje de despedida personalizado
+  if (text.startsWith('.setbye') && isAdmin) {
+    let byeMessage = text.slice(8).trim();
+    chat.customBye = byeMessage;
+    await conn.sendMessage(m.chat, 'Mensaje de despedida configurado con éxito.', 'conversation');
+  }
+
+  // Mensaje de bienvenida
   if (chat.bienvenida && m.messageStubType == 27) {
-    let bienvenida = `@${m.messageStubParameters[0].split`@`[0]} *BIENVENIDO A* ${groupMetadata.subject}\n\n*PUEDE LEER LAS REGLAS DEL GRUPO Y ASÍ EVITAR QUE TE ELIMINEN*\n\n${groupMetadata.desc}`;
+    let bienvenida = chat.customWelcome 
+      ? chat.customWelcome.replace('@user', `@${m.messageStubParameters[0].split`@`[0]}`).replace('@group', groupMetadata.subject)
+      : `@${m.messageStubParameters[0].split`@`[0]} *BIENVENIDO A* ${groupMetadata.subject}\n\n*PUEDE LEER LAS REGLAS DEL GRUPO Y ASÍ EVITAR QUE TE ELIMINEN*\n${groupMetadata.desc}`;
     
     await conn.sendAi(m.chat, botname, textbot, bienvenida, img, img, canal, estilo);
   }
-  
-  if (chat.bienvenida && m.messageStubType == 28) {
-    let bye = `@${m.messageStubParameters[0].split`@`[0]} *SE FUE DEL GRUPO*`;
+
+  // Mensaje de despedida
+  if (chat.bienvenida && (m.messageStubType == 28 || m.messageStubType == 32)) {
+    let bye = chat.customBye 
+      ? chat.customBye.replace('@user', `@${m.messageStubParameters[0].split`@`[0]}`)
+      : `@${m.messageStubParameters[0].split`@`[0]} *SE FUE DEL GRUPO*`;
     
     await conn.sendAi(m.chat, botname, textbot, bye, img, img, canal, estilo);
-  }
-  
-  if (chat.bienvenida && m.messageStubType == 32) {
-    let kick = `@${m.messageStubParameters[0].split`@`[0]} *SE FUE DEL GRUPO*`;
-    
-    await conn.sendAi(m.chat, botname, textbot, kick, img, img, canal, estilo);
   }
 }
